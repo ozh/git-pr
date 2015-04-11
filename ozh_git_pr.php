07e24f930ddc334280daa84191a328dddf96a333
ozh <ozh@ozh.org>
2015-04-11 21:21:09 +0200

    Escape shell command
    Just to be paranoid, we're not going to always trust github's return -- hat tip @tribut

--- ozh_git_pr.php
+++ ozh_git_pr.php
@@ -191,7 +191,7 @@ class Ozh_Git_PR{
      * @return $array  command output
      */
     function exec_and_maybe_continue( $cmd, $display = true ) {
-        exec( $cmd . " 2>&1", $output );
+        exec( escapeshellcmd( $cmd ) . " 2>&1", $output );
         
         if( $display ) {
             echo implode( "\n", $output ) . "\n";
